11de4e0342fd4a820508eca44e450c204ee78718
Abiola Fahad Ogunnaike <realabiolafahad@gmail.com>
2024-08-08 23:04:15 +0100

    URL Parameters & Delete Requests in the making... Too tired to continue now, I'll continue later In Sha'a Llah.

--- Backend/First API/server.js
+++ Backend/First API/server.js
@@ -1,5 +1,33 @@
 var expressi = require('express');
 var appi = expressi();
+var bodyParseri = require('body-parser');
+
+appi.use(bodyParseri.json());
+appi.use(bodyParseri.urlencoded({extended: false}));
+
+var awonEroja = 
+[
+    {
+        "id": "esd584",
+        "text": "Eja"
+    },
+    
+    {
+        "id": "jva582",
+        "text": "Miliki"
+    },
+    
+    {
+        "id": "dkv936",
+        "text": "Iyo"
+    },
+    
+    {
+        "id": "uaf306",
+        "text": "Iru"
+    }
+    
+]
 
 appi.get('/', function(request, response){
     response.send('This is supposed to be my 1st API ! Can you see this ?');
@@ -7,6 +35,10 @@ appi.get('/', function(request, response){
 
 appi.get('/page2', function(request, response){
     response.send('This is the second page, or is it API ?');
+});
+
+appi.get('/ingredients', function(request, response){
+    response.send(awonEroja);
 });
 
 appi.listen(3000, function()
